03eaa23c1de57b8b85b4c17deb6a1ec291e3262a
antonyks4506 <antonyks4506@gmail.com>
2025-10-27 12:24:09 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+import EmployeeForm from './components/EmployeeForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/employee-form" element={<EmployeeForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
